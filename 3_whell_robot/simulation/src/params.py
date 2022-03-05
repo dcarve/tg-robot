1082c24d3c03389a1cd2ac783bbcef1e94663648
dcarve <daniel.ermelino.carvalho@gmail.com>
2022-03-05 16:17:33 -0300

    add requirements

---
+++ 3_whell_robot/simulation/src/params.py
@@ -0,0 +1,27 @@
+import ctypes
+
+
+class whell_1:
+    angular_position_whell = 0
+    angular_speed_diretion = angular_position_whell - 90
+    radius = 1
+    center_offset = 1
+
+class whell_2:
+    angular_position_whell = 120
+    angular_speed_diretion = angular_position_whell - 90
+    radius = 1
+    center_offset = 1
+
+
+class whell_3:
+    angular_position_whell = 240
+    angular_speed_diretion = angular_position_whell - 90
+    radius = 1
+    center_offset = 1
+
+
+class Positions:
+
+
+    image_size
